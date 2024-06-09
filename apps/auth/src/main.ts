af90b5ed19bebca3db679b1312992f69e3858c31
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-06-09 22:25:58 +0530

    Auth service ready to listen for both incoming http requests as well as TCP connections

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,15 +4,19 @@ import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+
+  app.connectMicroservice({ transport: Transport.TCP });
 
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
 
   const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
