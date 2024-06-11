2619d0d1f5f964ed08ae0517459689e2d7aba7a9
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-06-11 13:09:28 +0530

    moved @CurrentUser() decorator to common library

--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -11,7 +11,7 @@ import {
 import { ReservationsService } from './reservations.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
-import { CommonAuthGuard } from '@app/common';
+import { CommonAuthGuard, CurrentUser, UserDto } from '@app/common';
 
 @Controller('reservations')
 export class ReservationsController {
@@ -19,22 +19,25 @@ export class ReservationsController {
 
   @Post()
   @UseGuards(CommonAuthGuard)
-  create(@Body() createReservationDto: CreateReservationDto) {
-    return this.reservationsService.create(createReservationDto);
+  async create(
+    @Body() createReservationDto: CreateReservationDto,
+    @CurrentUser() user: UserDto,
+  ) {
+    return this.reservationsService.create(createReservationDto, user._id);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.reservationsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string) {
     return this.reservationsService.findOne(id);
   }
 
   @Patch(':id')
-  update(
+  async update(
     @Param('id') id: string,
     @Body() updateReservationDto: UpdateReservationDto,
   ) {
@@ -42,7 +45,7 @@ export class ReservationsController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string) {
     return this.reservationsService.remove(id);
   }
 }
--- libs/common/src/auth/common-auth.guard.ts
+++ libs/common/src/auth/common-auth.guard.ts
@@ -7,6 +7,7 @@ import {
 import { Observable, map, tap } from 'rxjs';
 import { AUTH_SERVICE } from '../constants/services';
 import { ClientProxy } from '@nestjs/microservices';
+import { UserDto } from '../dto';
 
 @Injectable()
 export class CommonAuthGuard implements CanActivate {
@@ -22,7 +23,7 @@ export class CommonAuthGuard implements CanActivate {
     }
 
     return this.authClient
-      .send('authenticate', {
+      .send<UserDto>('authenticate', {
         Authentication: jwt,
       })
       .pipe(
