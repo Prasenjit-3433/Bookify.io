33fff3e823fedaa69a806de9cc53e208124d77c4
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-06-13 09:10:56 +0530

    configuring and starting payments microservice

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { PaymentsModule } from './payments.module';
+import { Transport } from '@nestjs/microservices';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(PaymentsModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('PORT'),
+    },
+  });
+
+  app.useLogger(app.get(Logger));
+  await app.startAllMicroservices();
 }
 bootstrap();
