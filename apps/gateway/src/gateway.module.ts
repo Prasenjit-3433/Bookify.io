02c43105108baea4fc2877c5194b453e8933e0b6
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-08-08 13:42:34 +0530

    configured and setup graphql module for reservations and auth service

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { LoggerModule } from '@app/common';
+import { IntrospectAndCompose } from '@apollo/gateway';
 
 @Module({
-  imports: [],
-  controllers: [GatewayController],
-  providers: [GatewayService],
+  imports: [
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      useFactory: (configService: ConfigService) => ({
+        gateway: {
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+              {
+                name: 'reservations',
+                url: configService.getOrThrow('RESERVATIONS_GRAPHQL_URL'),
+              },
+            ],
+          }),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    LoggerModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class GatewayModule {}
