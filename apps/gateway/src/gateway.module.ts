e467f98a360a83452a162ae81c676dc7072514a7
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-08-09 12:18:14 +0530

    pulled auth context from auth service to pupulate user object on request header

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
-import { LoggerModule } from '@app/common';
-import { IntrospectAndCompose } from '@apollo/gateway';
+import { AUTH_SERVICE, LoggerModule } from '@app/common';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { authContext } from './auth.context';
 
 @Module({
   imports: [
     GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
       useFactory: (configService: ConfigService) => ({
+        server: {
+          context: authContext,
+        },
         gateway: {
           supergraphSdl: new IntrospectAndCompose({
             subgraphs: [
@@ -19,10 +24,34 @@ import { IntrospectAndCompose } from '@apollo/gateway';
               },
             ],
           }),
+          buildService({ url }) {
+            return new RemoteGraphQLDataSource({
+              url,
+              willSendRequest({ request, context }) {
+                request.http.headers.set(
+                  'user',
+                  context.user ? JSON.stringify(context.user) : null,
+                );
+              },
+            });
+          },
         },
       }),
       inject: [ConfigService],
     }),
+    ClientsModule.registerAsync([
+      {
+        name: AUTH_SERVICE,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configService.getOrThrow('AUTH_HOST'),
+            port: configService.getOrThrow('AUTH_PORT'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
